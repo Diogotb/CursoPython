7c951a39e3212294de280ea26da441dcaf8b9a08
Diogotb <digogo@gmail.com>
2024-06-12 16:02:43 -0300

    12/06

--- Aula19/AnaliseDadosStreamlit/app.py
+++ Aula19/AnaliseDadosStreamlit/app.py
@@ -4,7 +4,6 @@ import pandas as pd
 # Carregar dados de um arquivo CSV
 df = pd.read_csv('movies.csv')
 
-<<<<<<< HEAD
 
 # Exibir os primeiros registros do DataFrame
 st.write(df.head())
@@ -13,19 +12,7 @@ st.write(df.head())
 filtro = st.sidebar.selectbox('Selecione o Filtro',df.columns)
 
 valor = st.sidebar.text_input('Digite o valor do filtro')
-=======
-# Exibir os primeiros registros do DataFrame
-st.write(df.head())
-
-# Filtragem simples
-filtro = st.sidebar.selectbox('Selecione uma coluna para filtrar:', df.columns)
-valor = st.sidebar.text_input('Digite o valor a ser filtrado:')
->>>>>>> b833215a4d54b95b94e4d116ef54458795fff479
 
 # Aplicar o filtro ao DataFrame
 df_filtrado = df[df[filtro] == valor]
 st.write(df_filtrado)
-<<<<<<< HEAD
-=======
-
->>>>>>> b833215a4d54b95b94e4d116ef54458795fff479
--- Aula19/HelloStreamlit/app.py
+++ Aula19/HelloStreamlit/app.py
@@ -1,14 +1,6 @@
 import streamlit as st
 
-<<<<<<< HEAD
-
-st.title('Hello Streamlit')
-st.write('Bem-vindo ao curso de Streamlit!')
-=======
 st.title('Hello Streamlit')
 st.write('Bem-vindo ao curso de Streamlit!')
 
-st.title('Título da Aplicação')
-Cabeçalho: Adiciona um cabeça
 
->>>>>>> b833215a4d54b95b94e4d116ef54458795fff479
